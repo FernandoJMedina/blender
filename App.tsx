99982a35a4a6db75e76753cc3f7c225699c7a86c
Fernando Medina <fernandomedina@Fernandos-MacBook-Pro-M3-Max.local>
2024-10-11 07:23:19 -0300

    blender animation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { Dimensions } from "react-native";
+import {
+  FilamentScene,
+  FilamentView,
+  DefaultLight,
+  Model,
+  Camera,
+  useCameraManipulator,
+  Animator,
+  RenderCallback,
+  Float3,
+} from "react-native-filament";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import Animated, {
+  FadeIn,
+  SlideInUp,
+  useSharedValue,
+} from "react-native-reanimated";
 
-export default function App() {
+const viewHeight = Dimensions.get("window").height;
+
+function SceneView() {
+  const rotation = useSharedValue<Float3>([0, 0, 0]);
+
+  // const renderCallback: RenderCallback = useCallback(() => {
+  //   "worklet";
+
+  //   // Add a rotation of 1 degree every frame
+  //   const newY = rotation.value[1] + 0.01;
+
+  //   // We need to create a new array for the internal listeners to trigger
+  //   rotation.value = [0, newY, 0];
+  // }, [rotation]);
+
+  const cameraManipulator = useCameraManipulator({
+    orbitHomePosition: [0, 0, 3], // "Camera location"
+    targetPosition: [0, 1, 0], // "Looking at"
+    orbitSpeed: [0.003, 0.003],
+  });
+
+  const panGesture = Gesture.Pan()
+    .onBegin((event) => {
+      const yCorrected = viewHeight - event.translationY;
+      cameraManipulator?.grabBegin(event.translationX, yCorrected, false); // false means rotation instead of translation
+    })
+    .onUpdate((event) => {
+      const yCorrected = viewHeight - event.translationY;
+      cameraManipulator?.grabUpdate(event.translationX, yCorrected);
+    })
+    .onEnd(() => {
+      cameraManipulator?.grabEnd();
+    });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureDetector gesture={panGesture}>
+      <Animated.View style={{ flex: 1 }}>
+        <FilamentView style={{ flex: 1 }}>
+          {/* 💡 A light source, otherwise the scene will be black */}
+          <DefaultLight />
+
+          {/* 📦 A 3D model */}
+
+          <FilamentView
+          // renderCallback={(frameInfo) => {
+          //   "worklet";
+          //   console.log(frameInfo);
+          // }}
+          >
+            <Model
+              rotate={rotation.value}
+              source={require("./models/ninja.glb")}
+            >
+              <Animator animationIndex={0} />
+            </Model>
+          </FilamentView>
+
+          {/* 📹 A camera through which the scene is observed and projected onto the view */}
+          <Camera cameraManipulator={cameraManipulator} />
+        </FilamentView>
+      </Animated.View>
+    </GestureDetector>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <FilamentScene>
+        <SceneView />
+      </FilamentScene>
+    </GestureHandlerRootView>
+  );
+}
